361d13a714aaba56c08595350ee23da76dad39d9
joluxer <joluxer@users.noreply.github.com>
2024-09-01 21:24:10 +0200

    Introduce file attributes to git for cross-platform development. Update distclean script for making working copy pristine.
    On different execution and development platforms are different EOL
    characters. And even some files shall not at all converted. This can
    be controlled by .gitattributes. Starting with known settings.
    
    # Conflicts:
    #	editor
    #	matiec

--- distclean.sh
+++ distclean.sh
@@ -2,6 +2,9 @@
 
 OPENPLC_DIR="$(dirname "$(readlink -f "$0")")"
 
-git checkout -- ${OPENPLC_DIR}/editor/arduino/ ${OPENPLC_DIR}/matiec/
-git clean -fdx ${OPENPLC_DIR}/matiec/ ${OPENPLC_DIR}/editor/arduino
+cd "${OPENPLC_DIR}"
+git checkout -- ./ ./editor/ ./matiec/
+git clean -fdx -- ./ ./matiec/ ./editor/
+git submodule foreach --recursive 'git checkout -- ./'
+git submodule foreach --recursive 'git clean -fdx -- ./'
 rm -f ${OPENPLC_DIR}/openplc_editor.sh
